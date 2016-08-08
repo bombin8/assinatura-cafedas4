ce2e502bbe0d02482429798c241d4aaad0952560
Giovanni Pires da Silva <gio@bombin8.com.br>
2016-08-08 04:16:31 -0300

    Add sources.

---
+++ demo.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8">
+        <title>Assinatura Café das 4 - Demo</title>
+        <link rel="stylesheet" href="css/assinatura-cafedas4.css">
+        <link rel="stylesheet" href="css/demo.css">
+    </head>
+    <body>
+        <footer class="demo-footer">
+            <address>
+                <a aria-label="Café das 4" class="assinatura-cafedas4" href="//www.cafedas4.com" target="_blank" title="Desenvolvido por: Café das 4">
+                    <span aria-hidden="true" class="assinatura-cafedas4__img">
+                        <?php echo file_get_contents('img/assinatura-cafedas4.svg'); ?>
+                    </span>
+                </a>
+            </address>
+        </footer>
+    </body>
+</html>
